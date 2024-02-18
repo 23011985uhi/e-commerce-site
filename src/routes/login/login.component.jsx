ed5dbef7befdc943dc18752d0463ada67b89a0dc
Graeme <23011985uhi@gmail.com>
2024-02-18 22:08:26 +0000

    Finished sign in form with google login button

--- src/routes/login/login.component.jsx
+++ src/routes/login/login.component.jsx
@@ -1,9 +1,8 @@
 import {useEffect} from 'react';
 import {getRedirectResult} from 'firebase/auth';
-
 import {signInWithGooglePopup, createUserDocumentFromAuth,} from '../../utility/firebase/firebase.utils'
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
-
+import SignInForm from '../../components/sign-in-form/sign-in-form.component'
 
 const Login = () => {
   
@@ -14,7 +13,7 @@ const Login = () => {
   return (
     <div>
       <h1>Sign in page</h1>
-      <button onClick = {logGoogleUser}>Sign in with Google Popup</button>
+      <SignInForm />
       <SignUpForm />
     </div>
   )
